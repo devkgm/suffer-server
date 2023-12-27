4cd1f3fddf26329e2ae1f609c8f6038fc32a4724
devkgm <gm.kim333@gmail.com>
2023-12-27 17:41:06 +0900

    delete routing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
 const bodyParser = require("body-parser");
 const login = require("./router/login");
+
 require("dotenv").config();
 
 const app = express();
@@ -13,144 +13,6 @@ app.use(bodyParser.json());
 
 // 인증
 app.use("/login", login);
-
-// 모든 회사 조회
-app.get("/companies", async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM "COMPANY"');
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// 특정 회사 조회
-app.get("/companies/:id", async (req, res) => {
-    const companyId = req.params.id;
-    try {
-        const result = await pool.query(
-            'SELECT * FROM "COMPANY" WHERE "ID" = $1',
-            [companyId]
-        );
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// 새로운 회사 추가
-app.post("/companies", async (req, res) => {
-    const { name, owner } = req.body;
-    let date = Date.now();
-    try {
-        const result = await pool.query(
-            'INSERT INTO "COMPANY" ("NAME", "OWNER") VALUES ($1, $2) RETURNING *',
-            [name, owner]
-        );
-        res.status(201).json(result.rows[0]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// 회사 정보 업데이트
-app.put("/companies/:id", async (req, res) => {
-    const companyId = req.params.id;
-    const { name, owner } = req.body;
-    try {
-        const result = await pool.query(
-            'UPDATE "COMPANY" SET "NAME" = $1,  "OWNER" = $2 WHERE "ID" = $3 RETURNING *',
-            [name, owner, companyId]
-        );
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// 회사 삭제
-app.delete("/companies/:id", async (req, res) => {
-    const companyId = req.params.id;
-    try {
-        await pool.query('DELETE FROM "COMPANY" WHERE "ID" = $1', [companyId]);
-        res.status(204).json();
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// MEMBER 테이블에 대한 라우트 및 CRUD 작업
-app.get("/members", async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM "MEMBER"');
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-app.get("/members/:id", async (req, res) => {
-    const memberId = req.params.id;
-    try {
-        const result = await pool.query(
-            'SELECT * FROM "MEMBER" WHERE "MEMBER_ID" = $1',
-            [memberId]
-        );
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-app.post("/members", async (req, res) => {
-    const { name, email, company_id } = req.body;
-    try {
-        const result = await pool.query(
-            'INSERT INTO "MEMBER" ("NAME", "EMAIL", "COMPANY_ID") VALUES ($1, $2, $3) RETURNING *',
-            [name, email, company_id]
-        );
-        res.status(201).json(result.rows[0]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-app.put("/members/:id", async (req, res) => {
-    const memberId = req.params.id;
-    const { name, email, company_id } = req.body;
-    try {
-        const result = await pool.query(
-            'UPDATE "MEMBER" SET "NAME" = $1, "EMAIL" = $2, "COMPANY_ID" = $3 WHERE "MEMBER_ID" = $4 RETURNING *',
-            [name, email, company_id, memberId]
-        );
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-app.delete("/members/:id", async (req, res) => {
-    const memberId = req.params.id;
-    try {
-        const result = await pool.query(
-            'DELETE FROM "MEMBER" WHERE "MEMBER_ID" = $1',
-            [memberId]
-        );
-        res.status(204).json();
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
