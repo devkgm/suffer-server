2f2761bd3414573d6fe9a3699361f9a7b2681a50
devkgm <gm.kim333@gmail.com>
2023-12-28 23:28:16 +0900

    UPDATE QUERY

--- router/project.js
+++ router/project.js
@@ -5,17 +5,11 @@ const { verifyToken } = require("../module/tokenManager");
 
 router.post("/", async (req, res) => {
     try {
-        const { name, description } = req.body;
+        const { name } = req.body;
 
-        // PostgreSQL에 프로젝트 생성 쿼리 작성
-        const insertQuery =
-            "INSERT INTO projects (name, description) VALUES ($1, $2) RETURNING *";
-        const values = [name, description];
-
-        // 데이터베이스 쿼리 실행
+        const insertQuery = `INSERT INTO "PROJECT" (NAME) VALUES ($1) RETURNING *`;
+        const values = [name];
         const result = await db.query(insertQuery, values);
-
-        // 쿼리 결과 반환
         res.status(201).json(result.rows[0]);
     } catch (error) {
         console.error("프로젝트 생성 중 오류 발생:", error);
